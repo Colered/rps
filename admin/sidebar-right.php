b01035d36b32918e032f3b54263f7cc29035d766
unknown <deepali.kakkar@IBGGN8860-1-L.ibtechnology.com>
2015-05-13 17:54:38 +0530

    html for admin dashboard page

---
+++ admin/sidebar-right.php
@@ -0,0 +1,5 @@
+<div class="main-sidebar">
+	<div class="side-div"><u>General SPS CONFIG</u></div>
+	<div class="side-div"><u>See other Reports</u></div>
+	<div class="side-div align-center"><b>17</b><br>Days left to close SPG</div>
+</div>
