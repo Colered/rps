07753354db29045c776a36bd9e9f0898ad54fbee
Ravendra <ravin.ims@gmail.com>
2015-05-14 16:30:27 +0530

    added another report for student

---
+++ reports2.php
@@ -0,0 +1,97 @@
+<?php
+include('header.php');?>
+<div class="custtable_left fontstyles" style="margin-left:20px;width:70%;">
+	<table id="datatables1" class="display">
+		<thead>
+			<tr>
+				<th >Career Subjects Completed  </th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+			</tr><tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+			</tr>
+		</tbody>
+	</table>
+	
+	<table id="datatables2" class="display">
+		<thead>
+			<tr>
+				<th >Pending Career Subjects to complete </th>
+				<th >Status</th> 
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+				<td class="align-center">On Progress</td> 
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+				<td class="align-center">On Progress</td> 
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+				<td class="align-center">Pending</td> 
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+				<td class="align-center">PreSelected</td> 
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+				<td class="align-center">PreSelected</td> 
+			</tr>
+			<tr>
+				<td class="align-center"><span class="subject-heading-1"><a href="#">View More</a></span></td>
+				<td class="align-center">&nbsp;</td> 
+			</tr>
+		</tbody>
+	</table>
+	<table id="datatables1" class="display">
+		<thead>
+			<tr>
+				<th >Selected Subject Groups </th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td class="align-center">SBJCT011 - S-8:00-12:00 – A3- 310 -<br /> Prof. Julia Smith</td>
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT011 - S-8:00-12:00 – A3- 310 -<br /> Prof. Julia Smith</td>
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT011 - S-8:00-12:00 – A3- 310 -<br /> Prof. Julia Smith</td>
+			</tr>
+			<tr>
+				<td class="align-center">SBJCT011 - S-8:00-12:00 – A3- 310 -<br /> Prof. Julia Smith</td>
+			</tr><tr>
+				<td class="align-center">SBJCT011 - S-8:00-12:00 – A3- 310 -<br /> Prof. Julia Smith</td>
+			</tr>
+		</tbody>
+	</table>
+				 
+</div>
+
+<?php include('sidebar_right.php'); ?>
+<div class="main-sidebar">
+	<div class="side-div align-center"><span class="highFont">12</span><br>Credits Taken </div>
+	<div class="side-div align-center"><span class="highFont">208</span><br>Credits Pending</div>
+	<div class="side-div align-center"><span class="highFont">7</span><br>Credits Pre-selected</div>
+</div>
+<?php
+	  include('footer.php');
+?>
+
--- sidebar_right.php
+++ sidebar_right.php
@@ -1,5 +1,5 @@
 <div class="main-sidebar">
 	<div class="side-div"><u>Download PENSUM PDF</u></div>
 	<div class="side-div"><u>Calendar View</u></div>
-	<div class="side-div align-center"><b>17</b><br>Days left to close SPG</div>
+	<div class="side-div align-center"><span class="highFont">12</span><br>Days left to close SPG</div>
 </div>
