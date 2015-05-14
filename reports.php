91a66dd0839e7ed1c4e239ea346d6311055da1b7
Ravendra <ravin.ims@gmail.com>
2015-05-14 12:54:42 +0530

    added html for subject pre-selection and reports page

--- reports.php
+++ reports.php
@@ -1,84 +1,72 @@
 <?php
 include('header.php');?>
 <div class="custtable_left fontstyles" style="margin-left:20px;width:80%;">
-	<table id="datatables-left" class="display">
+	<table id="datatables" class="display">
 		<thead>
 			<tr>
-				<th >SUBJECTS CURRENTLY TAKING </th>
+				<th >Status</th>
+				<th >Subject Group / Class</th>                        
+				<th >Subject Name</th>
+				<th >Subject Schedule</th>
+				<th >Subject Room</th>
+				<th >Credits</th>
+				<th>Teachers </th>		
 			</tr>
 		</thead>
 		<tbody>
 			<tr>
-				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+				<td class="align-center"><strong>Pre-selected</strong></td>
+				<td class="align-center">SBJCT011 - T- 001</td>                        
+				<td class="align-center">SUBJECT 11</td>
+				<td class="align-center">S-8:00-12:00</td>
+				<td class="align-center">A3- 310</td>	
+				<td class="align-center">4</td>
+				<td class="align-center">Prof. Julia Smith </td>
 			</tr>
 			<tr>
-				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+				<td class="align-center"><strong>Pre-selected</strong></td>
+				<td class="align-center">SBJCT011 - T- 001</td>                        
+				<td class="align-center">SUBJECT 11</td>
+				<td class="align-center">S-8:00-12:00</td>
+				<td class="align-center">A3- 310</td>	
+				<td class="align-center">4</td>
+				<td class="align-center">Prof. Julia Smith </td>
 			</tr>
 			<tr>
-				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
+				<td class="align-center"><strong>Pre-selected</strong></td>
+				<td class="align-center">SBJCT011 - T- 001</td>                        
+				<td class="align-center">SUBJECT 11</td>
+				<td class="align-center">S-8:00-12:00</td>
+				<td class="align-center">A3- 310</td>	
+				<td class="align-center">4</td>
+				<td class="align-center">Prof. Julia Smith </td>
 			</tr>
 			<tr>
-				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
-			</tr><tr>
-				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
-			</tr><tr>
-				<td class="align-center">SBJCT01 <span class="subject-heading-1"><a href="#">See Details</a></span></td>
-			</tr>
-		</tbody>
-	</table>
-	
-	<table id="datatables-right" class="display">
-		<thead>
-			<tr>
-				<th >SUBJECT CODE</th>
-				<th >SUBJECT NAME</th>                        
-				<th >GROUPS AVAILABLE</th>
-				<th >SUBJECT PRE-REQUISITE</th>
-				<th >STUDENTS SUBSCRIBED</th>				
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td class="align-center">SBJCT01</td>
-				<td class="align-center">SUBJECT1</td>                        
-				<td class="align-center">4 <span class="subject-heading-1"><a href="#">See Groups</a></span></td>
-				<td class="align-center">SBJCTA, SBJCTB <span class="subject-heading-1"><a href="#">Edit</a></span></td>
-				<td class="align-center">59 <span class="subject-heading-1"><a href="#">See Group Distribution</a></span></td>	
+				<td class="align-center"><strong>Pre-selected</strong></td>
+				<td class="align-center">SBJCT011 - T- 001</td>                        
+				<td class="align-center">SUBJECT 11</td>
+				<td class="align-center">S-8:00-12:00</td>
+				<td class="align-center">A3- 310</td>	
+				<td class="align-center">4</td>
+				<td class="align-center">Prof. Julia Smith </td>
 			</tr>
 			<tr>
-				<td class="align-center">SBJCT01</td>
-				<td class="align-center">SUBJECT1</td>                        
-				<td class="align-center">4 <span class="subject-heading-1"><a href="#">See Groups</a></span></td>
-				<td class="align-center">SBJCTA, SBJCTB <span class="subject-heading-1"><a href="#">Edit</a></span></td>
-				<td class="align-center">59 <span class="subject-heading-1"><a href="#">See Group Distribution</a></span></td>		
+				<td class="align-center"><strong>Pre-selected</strong></td>
+				<td class="align-center">SBJCT011 - T- 001</td>                        
+				<td class="align-center">SUBJECT 11</td>
+				<td class="align-center">S-8:00-12:00</td>
+				<td class="align-center">A3- 310</td>	
+				<td class="align-center">4</td>
+				<td class="align-center">Prof. Julia Smith </td>
 			</tr>
 			<tr>
-				<td class="align-center">SBJCT01</td>
-				<td class="align-center">SUBJECT1</td>                        
-				<td class="align-center">4 <span class="subject-heading-1"><a href="#">See Groups</a></span></td>
-				<td class="align-center">SBJCTA, SBJCTB <span class="subject-heading-1"><a href="#">Edit</a></span></td>
-				<td class="align-center">59 <span class="subject-heading-1"><a href="#">See Group Distribution</a></span></td>	
-			</tr>
-			<tr>
-				<td class="align-center">SBJCT01</td>
-				<td class="align-center">SUBJECT1</td>                        
-				<td class="align-center">4 <span class="subject-heading-1"><a href="#">See Groups</a></span></td>
-				<td class="align-center">SBJCTA, SBJCTB <span class="subject-heading-1"><a href="#">Edit</a></span></td>
-				<td class="align-center">59 <span class="subject-heading-1"><a href="#">See Group Distribution</a></span></td>		
-			</tr>
-			<tr>
-				<td class="align-center">SBJCT01</td>
-				<td class="align-center">SUBJECT1</td>                        
-				<td class="align-center">4 <span class="subject-heading-1"><a href="#">See Groups</a></span></td>
-				<td class="align-center">SBJCTA, SBJCTB <span class="subject-heading-1"><a href="#">Edit</a></span></td>
-				<td class="align-center">59 <span class="subject-heading-1"><a href="#">See Group Distribution</a></span></td>	
-			</tr>
-			<tr>
-				<td class="align-center">SBJCT01</td>
-				<td class="align-center">SUBJECT1</td>                        
-				<td class="align-center">4 <span class="subject-heading-1"><a href="#">See Groups</a></span></td>
-				<td class="align-center">SBJCTA, SBJCTB <span class="subject-heading-1"><a href="#">Edit</a></span></td>
-				<td class="align-center">59 <span class="subject-heading-1"><a href="#">See Group Distribution</a></span></td>		
+				<td class="align-center"><strong>Pre-selected</strong></td>
+				<td class="align-center">SBJCT011 - T- 001</td>                        
+				<td class="align-center">SUBJECT 11</td>
+				<td class="align-center">S-8:00-12:00</td>
+				<td class="align-center">A3- 310</td>	
+				<td class="align-center">4</td>
+				<td class="align-center">Prof. Julia Smith </td>
 			</tr>
 		</tbody>
 	</table>			 
