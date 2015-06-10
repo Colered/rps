1a5cc50360a94a3ddeb7bcd517bf14163be5741f
unknown <deepali.kakkar@IBGGN8860-1-L.ibtechnology.com>
2015-06-10 18:01:07 +0530

    changes in import csv functionality

--- postdata_import.php
+++ postdata_import.php
@@ -53,7 +53,7 @@ if (isset($_POST['form_action']) && $_POST['form_action']!=""){
 			foreach($dataArr as $values){
 				//check if file headers are in expected format
 				if($count == 1){
-					if(strtolower(trim($values[0]))=="course" && strtolower(trim($values[1]))=="course code" && strtolower(trim($values[2]))=="batch" && strtolower(trim($values[3]))=="subject" && strtolower(trim($values[4]))=="subject pre-requistie" && strtolower(trim($values[5]))=="maximum students" && strtolower(trim($values[6]))=="minimum students" &&  strtolower(trim($values[7]))=="subject costs" &&  strtolower(trim($values[8]))=="status"){
+					if(strtolower(trim($values[0]))=="course" && strtolower(trim($values[1]))=="course code" && strtolower(trim($values[2]))=="batch" && strtolower(trim($values[3]))=="subject" && strtolower(trim($values[4]))=="subject code" && strtolower(trim($values[5]))=="subject pre-requistie"  && strtolower(trim($values[6]))=="subject pre-requistie code" && strtolower(trim($values[7]))=="maximum students" && strtolower(trim($values[8]))=="minimum students" &&  strtolower(trim($values[9]))=="subject costs" &&  strtolower(trim($values[10]))=="status"){
 						//File format is correct
 					}else{
 						$errorArr[] = "File format is not same, one or more header names are not matching";
@@ -62,7 +62,7 @@ if (isset($_POST['form_action']) && $_POST['form_action']!=""){
 						exit;
 					}
 					$count++;
-				}elseif(strtolower(trim($values[0]))!="" && strtolower(trim($values[1]))!="" && strtolower(trim($values[2]))!="" && strtolower(trim($values[3]))!="" && strtolower(trim($values[4]))!="" && strtolower(trim($values[5]))!="" && strtolower(trim($values[6]))!="" && strtolower(trim($values[7]))!="" &&  strtolower(trim($values[8]))!=""){
+				}elseif(strtolower(trim($values[0]))!="" && strtolower(trim($values[1]))!="" && strtolower(trim($values[2]))!="" && strtolower(trim($values[3]))!="" && strtolower(trim($values[4]))!="" && strtolower(trim($values[5]))!="" && strtolower(trim($values[6]))!="" && strtolower(trim($values[7]))!="" &&  strtolower(trim($values[8]))!="" &&  strtolower(trim($values[9]))!="" &&  strtolower(trim($values[10]))!="") {
 					//check if course name exist
 					$coursekey =array_search(trim(strtolower($values[1])), array_map('trim', array_map('strtolower', $courseCodeArr)));
 					if(($coursekey === 0) || ($coursekey > 0)){
@@ -84,9 +84,9 @@ if (isset($_POST['form_action']) && $_POST['form_action']!=""){
 					}else{
 						$errorArr[] = "Error in Row no:" .$count." This Batch Name does not exist in the Course";
 					}
-					if ((isset($values[2]) && $values[2]!='') && (isset($values[3]) && $values[3]!='')){
-						$eg_name = iconv('UTF-8', 'ISO-8859-1',$values[3]);
-						$resultQRY = mysqli_query($db_FED, "SELECT eg.id FROM elective_groups eg inner join batches b on b.id = eg.batch_id inner join courses c on c.id = b.course_id WHERE b.course_id='".$course_id."' and b.name = '".trim($values[2])."' and eg.school_id='".$_SESSION['school_id']."' and eg.name='". trim($eg_name)."' LIMIT 1");
+					//check if subject exist
+					if ((isset($values[2]) && $values[2]!='') && (isset($values[3]) && $values[3]!='') && (isset($values[4]) && $values[4]!='')){
+						$resultQRY = mysqli_query($db_FED, "SELECT s.id FROM subjects s WHERE batch_id = '".$batch_id."' and school_id='".$_SESSION['school_id']."' and s.name='".trim($values[3])."' and s.code='".trim($values[4])."' LIMIT 1");
 						$dRowQ = mysqli_fetch_assoc($resultQRY);
 						if(count($dRowQ) > 0){
 							$subject_id = $dRowQ['id'];
@@ -94,15 +94,15 @@ if (isset($_POST['form_action']) && $_POST['form_action']!=""){
 							$errorArr[] = "Error in Row no:" .$count." Subject is not associated to given course and batch";
 						}
 					}
-					if (isset($values[4]) && $values[4]!='')
+					//check if required subject exist
+					if ((isset($values[2]) && $values[2]!='') && (isset($values[5]) && $values[5]!='') && (isset($values[6]) && $values[6]!=''))
 					{
 						$cnt = 0;
 						$subject_id_arr = array();
-						$req_sub_arr = explode(",",trim($values[4]));
+						$req_sub_arr = explode(",",trim($values[6]));
 						foreach($req_sub_arr as $req_sub)
 						{
-							$eg_name = iconv('UTF-8', 'ISO-8859-1',$req_sub);
-							$resultQRY = mysqli_query($db_FED, "SELECT eg.id FROM elective_groups eg inner join batches b on b.id = eg.batch_id inner join courses c on c.id = b.course_id WHERE b.course_id='".$course_id."' and b.name = '".trim($values[2])."' and eg.school_id='".$_SESSION['school_id']."' and eg.name='".trim($eg_name)."' LIMIT 1");
+							$resultQRY = mysqli_query($db_FED, "SELECT s.id FROM subjects s WHERE batch_id ='".$batch_id."' and school_id='".$_SESSION['school_id']."' and s.code='".trim($req_sub)."' LIMIT 1");
 							$dRowQ = mysqli_fetch_assoc($resultQRY);
 							if(count($dRowQ) > 0){
 								$subject_id_arr[] = $dRowQ['id'];
@@ -117,12 +117,11 @@ if (isset($_POST['form_action']) && $_POST['form_action']!=""){
 					$count++;
 				}
 			}
-			//echo $subject_id;die;
 			//if file have no errors create activity and sessions else return error messages array
 			if(count($errorArr)==0){
 				$total = 0;
 				foreach($dataArr as $values){
-						if($total > 0 && strtolower(trim($values[0]))!="" && strtolower(trim($values[1]))!="" && strtolower(trim($values[2]))!="" && strtolower(trim($values[3]))!="" && strtolower(trim($values[4]))!="" && strtolower(trim($values[5]))!="" && strtolower(trim($values[6]))!="" && strtolower(trim($values[7]))!="" && strtolower(trim($values[8]))!=""){
+						if($total > 0 && strtolower(trim($values[0]))!="" && strtolower(trim($values[1]))!="" && strtolower(trim($values[2]))!="" && strtolower(trim($values[3]))!="" && strtolower(trim($values[4]))!="" && strtolower(trim($values[5]))!="" && strtolower(trim($values[6]))!="" && strtolower(trim($values[7]))!="" && strtolower(trim($values[8]))!="" &&  strtolower(trim($values[9]))!="" &&  strtolower(trim($values[10]))!=""){
 								//get the course_id
 								$coursekey =array_search(trim(strtolower($values[1])), array_map('trim', array_map('strtolower', $courseCodeArr)));
 								if(($coursekey === 0) || ($coursekey > 0)){
@@ -141,19 +140,17 @@ if (isset($_POST['form_action']) && $_POST['form_action']!=""){
 									 $batch_id = $batchIDArr[$batchNamekey];
 								}
 								//get the subject id
-								$eg_name = iconv('UTF-8', 'ISO-8859-1',$values[3]);
-								$resultQRY = mysqli_query($db_FED, "SELECT eg.id FROM elective_groups eg inner join batches b on b.id = eg.batch_id inner join courses c on c.id = b.course_id WHERE b.course_id='".$course_id."' and b.name = '".trim($values[2])."' and eg.school_id='".$_SESSION['school_id']."' and eg.name='". trim($eg_name)."' LIMIT 1");
+								$resultQRY = mysqli_query($db_FED, "SELECT s.id FROM subjects s WHERE batch_id = '".$batch_id."' and school_id='".$_SESSION['school_id']."' and s.name='".trim($values[3])."' and s.code='".trim($values[4])."' LIMIT 1");
 								$dRowQ = mysqli_fetch_assoc($resultQRY);
 								if(count($dRowQ) > 0){
 									$subject_id = $dRowQ['id'];
 								}
 								//get the required subject id
 								$subject_id_arr = array();
-								$req_sub_arr = explode(",",trim($values[4]));
+								$req_sub_arr = explode(",",trim($values[6]));
 								foreach($req_sub_arr as $req_sub)
 								{
-									$eg_name = iconv('UTF-8', 'ISO-8859-1',$req_sub);
-									$resultQRY = mysqli_query($db_FED, "SELECT eg.id FROM elective_groups eg inner join batches b on b.id = eg.batch_id inner join courses c on c.id = b.course_id WHERE b.course_id='".$course_id."' and b.name = '".trim($values[2])."' and eg.school_id='".$_SESSION['school_id']."' and eg.name='".trim($eg_name)."' LIMIT 1");
+									$resultQRY = mysqli_query($db_FED, "SELECT s.id FROM subjects s WHERE batch_id ='".$batch_id."' and school_id='".$_SESSION['school_id']."' and s.code='".trim($req_sub)."' LIMIT 1");
 									$dRowQ = mysqli_fetch_assoc($resultQRY);
 									if(count($dRowQ) > 0){
 										$subject_id_arr[] = $dRowQ['id'];										
@@ -172,16 +169,18 @@ if (isset($_POST['form_action']) && $_POST['form_action']!=""){
 																				 batch_name = '".mysql_real_escape_string(trim($values[2]))."',
 																				 subject_id = '".$subject_id."',
 																				 subject_name = '".mysql_real_escape_string(trim($values[3]))."',
+																				 subject_code = '".mysql_real_escape_string(trim($values[4]))."',
 																				 required_subject_id = '".$subject_id_str."',
-																				 required_subject_name = '".mysql_real_escape_string(trim($values[4]))."',
-																				 max_students = '".mysql_real_escape_string(trim($values[5]))."',
-																				 min_students = '".mysql_real_escape_string(trim($values[6]))."',
-																				 cost = '".mysql_real_escape_string(trim($values[7]))."',
+																				 required_subject_name = '".mysql_real_escape_string(trim($values[5]))."',
+																				 required_subject_code = '".mysql_real_escape_string(trim($values[6]))."',
+																				 max_students = '".mysql_real_escape_string(trim($values[7]))."',
+																				 min_students = '".mysql_real_escape_string(trim($values[8]))."',
+																				 cost = '".mysql_real_escape_string(trim($values[9]))."',
 																				 school_id = '".$_SESSION['school_id']."',
-																				 status = '".mysql_real_escape_string(trim($values[8]))."' where id='".$dRowQ['id']."'";
+																				 status = '".mysql_real_escape_string(trim($values[10]))."' where id='".$dRowQ['id']."'";
 									mysqli_query($db_RPS,$sql_upd);
 								}else{									
-									$result = mysqli_query($db_RPS, "INSERT INTO subjects_prerequistie(course_id,course_name,course_code,batch_id,batch_name,subject_id,subject_name,required_subject_id,required_subject_name,max_students,min_students,cost,school_id,status) VALUES ('" .$course_id. "', '" .mysql_real_escape_string(trim($values[0])). "','" .mysql_real_escape_string(trim($values[1])). "', '" .$batch_id. "', '" .mysql_real_escape_string(trim($values[2])). "', '" .$subject_id. "', '" .mysql_real_escape_string(trim($values[3])). "', '" .$subject_id_str. "', '" .mysql_real_escape_string(trim($values[4])). "', '" .mysql_real_escape_string(trim($values[5])). "', '" .mysql_real_escape_string(trim($values[6])). "', '" .mysql_real_escape_string(trim($values[7])). "','".$_SESSION['school_id']."','" .mysql_real_escape_string(trim($values[8])). "');");
+									$result = mysqli_query($db_RPS, "INSERT INTO subjects_prerequistie(course_id,course_name,course_code,batch_id,batch_name,subject_id,subject_name,subject_code,required_subject_id,required_subject_name,required_subject_code,max_students,min_students,cost,school_id,status) VALUES ('" .$course_id. "', '" .mysql_real_escape_string(trim($values[0])). "','" .mysql_real_escape_string(trim($values[1])). "', '" .$batch_id. "', '" .mysql_real_escape_string(trim($values[2])). "', '" .$subject_id. "','".mysql_real_escape_string(trim($values[3]))."', '" .mysql_real_escape_string(trim($values[4])). "', '" .$subject_id_str. "', '".mysql_real_escape_string(trim($values[5]))."', '" .mysql_real_escape_string(trim($values[6])). "', '" .mysql_real_escape_string(trim($values[7])). "', '" .mysql_real_escape_string(trim($values[8])). "', '" .mysql_real_escape_string(trim($values[9])). "','".$_SESSION['school_id']."','" .mysql_real_escape_string(trim($values[10])). "');");
 								}								
 						} $total++ ; 
 				}
